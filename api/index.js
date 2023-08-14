f22271ccc93b79abb3a66346ad427b69828885e9
ades-ship <adeshsengar156@gmail.com>
2023-08-14 16:18:56 +0530

    add backend

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
-import Express  from "express";
-const app=Express();
+import express  from "express";
+const app=express();
 import userRoutes from "./routes/users.js"
 import authRoutes from "./routes/auth.js"
-import postRoutes from "./routes/post.js"
+import postRoutes from "./routes/posts.js"
 import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
-
+import cookieParser from "cookie-parser";
+import cors from "cors"
+// middleware
+app.use(express.json())
+app.use(cors)
+app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
